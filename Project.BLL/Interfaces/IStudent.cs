a2316f5d109b1847616857a6b1b168285526332c
ibraheem Zeer <137792310+ibraheem-zeer@users.noreply.github.com>
2024-11-30 02:35:15 +0200

    BLL IStudent & ITeacher

---
+++ Project.BLL/Interfaces/IStudent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.Interfaces
+{
+    internal interface IStudent
+    {
+        void Login(string Email, string Password);
+        void Enroll(int id);
+        void DoExam(int id);
+        int SeeResult(int id);
+    }
+}
---
+++ Project.BLL/Interfaces/ITeacher.cs
@@ -0,0 +1,18 @@
+﻿using Project.DAL.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.Interfaces
+{
+    internal interface ITeacher
+    {
+        void Login(string Email, string Password);
+        ICollection<Exam> ViewExams(Exam exam);
+        void CompleteExam(int id);
+        ICollection<Course> ViewAssignedCourse();
+        int ViewResultOfExam(int id);
+    }
+}
