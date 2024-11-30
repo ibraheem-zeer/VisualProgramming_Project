31b7cbb88c71e9a1786b9900e987f81fa0be39cb
ibraheem Zeer <137792310+ibraheem-zeer@users.noreply.github.com>
2024-11-30 02:28:28 +0200

    BLL IAdmin

---
+++ Project.BLL/Interfaces/IAdmin.cs
@@ -0,0 +1,36 @@
+﻿using Project.DAL.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.Interfaces
+{
+    public interface IAdmin
+    {
+        ICollection<Student> GetAllStudents();
+        ICollection<Teacher> GetAllTeachers();
+        ICollection<Course> GetAllCourses();
+        ICollection<Exam> GetAllExams();
+
+        Student GetStudent(int id);
+        Teacher GetTeacher(int id);
+        Course GetCourse(int id);
+        Exam GetExam(int id);
+
+        int CreateStudent(Student student);
+        int UpdateStudent(Student student);
+        int DeleteStudent(Student student);
+        int CreateTeacher(Teacher teacher);
+        int UpdateTeacher(Teacher teacher);
+        int DeleteTeacher(Teacher teacher);
+        int CreateCourse(Course course);
+        int UpdateCourse(Course course);
+        int DeleteCourse(Course course);
+        int CreateExam(Exam exam);
+        int UpdateExam(Exam exam);
+        int DeleteExam(Exam exam);
+        int ViewResult();
+    }
+}
